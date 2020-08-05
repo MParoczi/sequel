480bf79665db938934f59fb384a0e61383aee425
Márk Paróczi <mark.paroczi@gmail.com>
2020-08-05 20:04:32 +0200

    Add IRepositoryWrapper interface

---
+++ Sequel/Contexts/Contracts/IRepositoryWrapper.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+
+namespace Sequel.Contexts.Contracts
+{
+    public interface IRepositoryWrapper
+    {
+        public IGenreRepository Genre { get; set; }
+
+        public IShowRepository Show { get; set; }
+
+        public IShowGenreRepository ShowGenre { get; set; }
+
+        public ISeasonRepository Season { get; set; }
+
+        public IEpisodeRepository Episode { get; set; }
+
+        public IActorRepository Actor { get; set; }
+
+        public IShowCharacterRepository ShowCharacter { get; set; }
+
+        Task Save();
+    }
+}
