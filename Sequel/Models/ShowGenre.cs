e05c8aae00decf6fa8fb98133c77e9d142a01d31
Márk Paróczi <mark.paroczi@gmail.com>
2020-08-05 16:45:15 +0200

    Add ShowGenre model class

---
+++ Sequel/Models/ShowGenre.cs
@@ -0,0 +1,15 @@
+﻿namespace Sequel.Models
+{
+    public class ShowGenre
+    {
+        public int ShowGenreId { get; set; }
+
+        public int ShowId { get; set; }
+
+        public Show Show { get; set; }
+
+        public int GenreId { get; set; }
+
+        public Genre Genre { get; set; }
+    }
+}
