325bf4f89dce361299d0e2357cb57710552d76b9
Márk Paróczi <mark.paroczi@gmail.com>
2020-08-05 16:55:36 +0200

    Add ShowCharacter model class

---
+++ Sequel/Models/ShowCharacter.cs
@@ -0,0 +1,17 @@
+﻿namespace Sequel.Models
+{
+    public class ShowCharacter
+    {
+        public int ShowCharacterId { get; set; }
+
+        public int ShowId { get; set; }
+
+        public Show Show { get; set; }
+
+        public int ActorId { get; set; }
+
+        public Actor Actor { get; set; }
+
+        public string CharacterName { get; set; }
+    }
+}
