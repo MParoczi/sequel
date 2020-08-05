8390780c1d772571c33518b996912c3e58215520
Márk Paróczi <mark.paroczi@gmail.com>
2020-08-05 16:51:25 +0200

    Add Episode model class

---
+++ Sequel/Models/Episode.cs
@@ -0,0 +1,17 @@
+﻿namespace Sequel.Models
+{
+    public class Episode
+    {
+        public int EpisodeId { get; set; }
+
+        public string Title { get; set; }
+
+        public int EpisodeNumber { get; set; }
+
+        public string Overview { get; set; }
+
+        public int SeasonId { get; set; }
+
+        public Season Season { get; set; }
+    }
+}
