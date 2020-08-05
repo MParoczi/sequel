d2ba42e4b4eac71e45afbceb3399a627d8ba6aae
Márk Paróczi <mark.paroczi@gmail.com>
2020-08-05 16:53:31 +0200

    Add Actor model class

---
+++ Sequel/Models/Actor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Sequel.Models
+{
+    public class Actor
+    {
+        public int ActorId { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public DateTime Death { get; set; }
+
+        public string Biography { get; set; }
+    }
+}
