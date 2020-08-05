9291ccb12cd1c9359221a6d3e9e300669d5e4d7b
Márk Paróczi <mark.paroczi@gmail.com>
2020-08-05 19:35:06 +0200

    Add ServiceExtension static class

---
+++ Sequel/Extensions/ServiceExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
+using Sequel.Contexts;
+
+namespace Sequel.Extensions
+{
+    public static class ServiceExtension
+    {
+        public static void ConfigureSqlConnection(this IServiceCollection services)
+        {
+            var connectionString = Environment.GetEnvironmentVariable("SEQUEL_CONNECTION_STRING");
+
+            services.AddDbContext<SequelContext>(cfg =>
+                cfg.UseNpgsql(connectionString ?? throw new NpgsqlException()));
+        }
+    }
+}
