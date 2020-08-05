5a238e9da00e67e7c2697f4386c53c92210452b1
Márk Paróczi <mark.paroczi@gmail.com>
2020-08-05 16:32:06 +0200

    Add Show model class

---
+++ Sequel/Models/Show.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Sequel.Models
+{
+    public class Show
+    {
+        public int ShowId { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime Year { get; set; }
+
+        public string Overview { get; set; }
+
+        public int Runtime { get; set; }
+
+        public string Trailer { get; set; }
+
+        public string Homepage { get; set; }
+
+        public double Rating { get; set; }
+    }
+}
