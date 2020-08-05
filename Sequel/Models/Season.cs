f12a764dba6d8a02c8849544cc2a87a716755b54
Márk Paróczi <mark.paroczi@gmail.com>
2020-08-05 16:47:36 +0200

    Add Season model class

---
+++ Sequel/Models/Season.cs
@@ -0,0 +1,17 @@
+﻿namespace Sequel.Models
+{
+    public class Season
+    {
+        public int SeasonId { get; set; }
+
+        public int SeasonNumber { get; set; }
+
+        public string Name { get; set; }
+
+        public string Overview { get; set; }
+
+        public int ShowId { get; set; }
+
+        public Show Show { get; set; }
+    }
+}
