1e310e0a18cfc6ae636cebc6cf42498dc6241c23
Márk Paróczi <mark.paroczi@gmail.com>
2020-08-06 15:00:08 +0200

    Add ShowController controller class

---
+++ Sequel/Controllers/ShowController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Sequel.Contexts.Contracts;
+using Sequel.Models;
+
+namespace Sequel.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ShowController : ControllerBase
+    {
+        public ShowController(IRepositoryWrapper repository)
+        {
+            Repository = repository;
+        }
+
+        private IRepositoryWrapper Repository { get; }
+
+        [HttpGet]
+        public IEnumerable<Show> Get()
+        {
+            return Repository.Show.FindAll();
+        }
+    }
+}
